456b3cfafed08924e86d72907ddad667966f5a95
Rohit Crasta <rohitcrasta@gmail.com>
2015-11-21 15:32:20 -0500

    misc level edits

--- src/global/Settings.js
+++ src/global/Settings.js
@@ -61,50 +61,35 @@ Settings = {
     */
     levels: [
 
-        //CUT LEVELS
-        // //'levels/startlevel0.json', - - CUT
-        // // 'levels/startlevel1.json', - - CUT
-        // 'levels/openlevel.json', - - CUT
-        // 'levels/openlevel2.json', - - CUT
-        // 'levels/passagelevel.json', - - CUT
-        // 'levels/startlevel2.json', - - CUT
-        // 'levels/startlevel4.json', - - CUT
-        // 'levels/startlevel_skinny.json', - - CUT
-        // 'levels/test_reorientation1.json', --CUT
-        // 'levels/test_spikedBlock.json',--CUT
-        // 'levels/test_spikedBall.json',
-        // 'levels/test_moveBlock.json', --CUT
-        // 'levels/test_breakable.json',--CUT
-
-        // 'levels/test_reorientation2.json',--TEMPREMOVE
-        // 'levels/teamlevel.json', --TEMPREMOVE
-        // 'levels/test_random1.json', --TEMPREMOVE
-
-        //ACTUAL test levels
-        // 'levels/testReal_01.json',
-
-        //TEST LOAD LEVEL
-        // 'levels/my_level.json',
+        //TEST
+        // 'levels/test/openlevel.json',
+        // 'levels/test/openlevel0.json',
+        // 'levels/test/startlevel4.json',
+        // 'levels/test/test_reorientation2.json',
 
         //Basic
         'levels/startlevel5.json',
         'levels/splitlevel.json',
         'levels/startlevel3.json',
 
-        'levels/test_reorientation0.json',
+        'levels/test/teamlevel.json',
+        'levels/test_reorientation0_v2.json',
         'levels/fliplevel.json',
+        'levels/test_reorientation3.json',
 
 
         'levels/enemylevel2.json',
         'levels/enemylevel1.json',
+
+        'levels/test/passagelevel.json',
 
         'levels/wallgatelevel.json',
         'levels/gatelevel.json',
         'levels/gatetunnellevel.json',
         'levels/test_colors1.json',
 
+        'levels/test/passagelevel.json',
         'levels/gates1.json',
-        // 'levels/openlevel0.json',
 
         'levels/test_breakable0.json',
         'levels/test_breakable1.json',
@@ -123,6 +108,5 @@ Settings = {
         'levels/test_movable3.json',
         'levels/test_movable1.json',
         'levels/test_moveBlock0.json',
-
     ]
 }
